f0e59a1b61e5ba1075304e83376e6c2c62aea540
hungpppiii <hungppp303@gmail.com>
2023-09-24 23:35:35 +0700

    Update entity model + base controller

---
+++ Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public class Course : IEntityBase
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(100)]
+        public string Gradle { get; set; }
+        [Range(0, double.MaxValue)]
+        public double Price { get; set; }
+        public string? Description { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public Guid TeacherId { get; set; }
+        public virtual User Teacher { get; set; }
+        public virtual ICollection<User> Students { get; set; } = new HashSet<User>();
+        public virtual ICollection<Lesson> Lessons { get; set; } = new HashSet<Lesson>();
+    }
+}
